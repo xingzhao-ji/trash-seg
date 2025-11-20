27861280501d814802c548916ec15d8d89ca8989
Itisalex2 <chenalexlu@gmail.com>
2025-11-20 09:39:54 -0800

    Set up initial frontend and backend files

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+function App() {
+  const [message, setMessage] = useState('');
+  useEffect(() => {
+    fetch('/api')
+      .then(response => response.text())
+      .then(data => setMessage(data));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>{message}</h1>
+    </div>
+  );
+}
+export default App;
